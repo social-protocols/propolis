daecc7b1697f8445154f5dbe60ca90b4ad98ba32
Thomass Karolski <thomas.karolski@gmail.com>
2023-02-13 18:48:48 +0100

    build index page with mini voting system

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,8 @@
 use dotenvy::dotenv;
 use std::env;
 use axum::{
-    routing::{get},
+    Form,
+    routing::{get, post},
     Router, Extension,
     http::StatusCode,
     Json, response::Html, extract::Path,
@@ -10,8 +11,8 @@ use axum::{
 use std::net::SocketAddr;
 use sqlx::{sqlite::SqlitePoolOptions, SqlitePool};
 
-use serde::Serialize;
-use askama::Template;
+use serde::{Deserialize, Serialize};
+use askama::{Template};
 
 // TODO: Login with user secret and cookie
 
@@ -27,6 +28,8 @@ async fn main() {
         .unwrap();
 
     let app = Router::new()
+        .route("/", get(index))
+        .route("/", post(index_post))
         .route("/next", get(root))
         .route("/hello/:name", get(hello))
         .layer(Extension(pool));
@@ -41,10 +44,14 @@ async fn main() {
 }
 
 
+#[derive(Template)]
+#[template(path = "index.j2")]
+struct IndexTemplate<'a> {
+    statement: &'a Statement,
+}
 
 #[derive(Template)]
 #[template(path = "hello.html")]
-
 struct HelloTemplate<'a> {
     name: &'a str,
 }
@@ -54,11 +61,54 @@ async fn hello(Path(name): Path<String>) -> Html<String> {
     Html(template.render().unwrap())
 }
 
-#[derive(Serialize)]
+#[derive(Serialize, sqlx::FromRow)]
 struct Statement {
     id: i64,
     text: String,
 }
+
+#[derive(Deserialize)]
+struct UserStatementSelection {
+    statement_id: i64,
+    selection: String
+}
+
+async fn index_post(
+    Extension(pool):Extension<SqlitePool>,
+    Form(selection): Form<UserStatementSelection>) -> Html<String>
+{
+    let user_id = 1;
+    let statement_id = 1;
+    let opinion = match selection.selection.as_str() {
+        "y" => { 1 }
+        "n" => { -1 }
+        _ => { 0 }
+    };
+
+    let query = sqlx::query!(
+        "INSERT INTO opinions (user_id, statement_id, opinion) VALUES (?, ?, ?)",
+        user_id, statement_id, opinion)
+        .execute(&pool).await;
+    query.expect("Database problem");
+
+    index(Extension(pool)).await
+}
+
+
+// Display one statement at random
+async fn index(Extension(pool):Extension<SqlitePool>) -> Html<String> {
+
+    let query = sqlx::query_as::<_, Statement>("SELECT id, text from statements ORDER BY RANDOM() LIMIT 1");
+    let result = query.fetch_one(&pool).await.expect("Must be valid");
+
+    let template = IndexTemplate {
+        statement: &result
+    };
+
+    Html(template.render().unwrap())
+
+}
+
 
 async fn root(Extension(pool):Extension<SqlitePool>) -> Result<Json<Statement>,StatusCode> {
     let row: Result<Statement, sqlx::Error> = sqlx::query_as!(Statement,"SELECT id, text from statements limit 1")
