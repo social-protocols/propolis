862609c3795145fe64166661906db0645860c97c
Felix Dietze <felix.dietze@rwth-aachen.de>
2023-02-14 22:26:28 +0100

    format

--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,21 @@
+use axum::{
+    extract::Path,
+    http::StatusCode,
+    response::Html,
+    routing::{get, post},
+    Extension, Form, Json, Router,
+};
 use dotenvy::dotenv;
 use std::env;
 use tower_cookies::{Cookie, CookieManagerLayer, Cookies};
-use axum::{
-    Form,
-    routing::{get, post},
-    Router, Extension,
-    http::StatusCode,
-    Json, response::Html, extract::Path,
-};
 
-use std::net::SocketAddr;
 use sqlx::{sqlite::SqlitePoolOptions, SqlitePool};
+use std::net::SocketAddr;
 
-use serde::{Deserialize, Serialize};
-use askama::{Template};
-use rand::{thread_rng, Rng};
+use askama::Template;
 use rand::distributions::Alphanumeric;
+use rand::{thread_rng, Rng};
+use serde::{Deserialize, Serialize};
 
 fn generate_secret() -> String {
     thread_rng()
@@ -42,18 +42,15 @@ async fn main() {
         .route("/", get(index))
         .route("/", post(index_post))
         .layer(Extension(pool))
-        .layer(CookieManagerLayer::new())
-        ;
+        .layer(CookieManagerLayer::new());
 
-    let addr = SocketAddr::from(([127, 0 , 0, 1], 8000));
+    let addr = SocketAddr::from(([127, 0, 0, 1], 8000));
     println!("listening on {}", addr);
     axum::Server::bind(&addr)
         .serve(app.into_make_service())
         .await
         .unwrap();
-
 }
-
 
 #[derive(Template)]
 #[template(path = "index.j2")]
@@ -75,16 +72,20 @@ struct UserStatementVote {
 
 async fn index_post(
     cookies: Cookies,
-    Extension(pool):Extension<SqlitePool>,
-    Form(vote): Form<UserStatementVote>) -> Html<String>
-{
+    Extension(pool): Extension<SqlitePool>,
+    Form(vote): Form<UserStatementVote>,
+) -> Html<String> {
     let user = ensure_auth(&cookies, &pool).await;
     let statement_id = vote.statement_id;
 
     let query = sqlx::query!(
         "INSERT INTO votes (user_id, statement_id, vote) VALUES (?, ?, ?)",
-        user.id, statement_id, vote.vote)
-        .execute(&pool).await;
+        user.id,
+        statement_id,
+        vote.vote
+    )
+    .execute(&pool)
+    .await;
     query.expect("Database problem");
 
     index(cookies, Extension(pool)).await
@@ -98,29 +99,32 @@ struct User {
 
 async fn create_user(pool: &SqlitePool) -> User {
     let secret = generate_secret();
-    let user = sqlx::query_as::<_, User>(
-        "INSERT INTO users (secret) VALUES (?) RETURNING id, secret")
-        .bind(secret)
-        .fetch_one(pool).await.expect("Must be valid");
+    let user =
+        sqlx::query_as::<_, User>("INSERT INTO users (secret) VALUES (?) RETURNING id, secret")
+            .bind(secret)
+            .fetch_one(pool)
+            .await
+            .expect("Must be valid");
 
     user
 }
 
-async fn ensure_auth(cookies: &Cookies,
-                     pool: &SqlitePool) -> User {
+async fn ensure_auth(cookies: &Cookies, pool: &SqlitePool) -> User {
     if let Some(secret) = cookies.get("secret") {
         let secret = secret.value().to_string();
-        let query = sqlx::query_as!(User, "SELECT id, secret from users WHERE secret = ?", secret);
+        let query = sqlx::query_as!(
+            User,
+            "SELECT id, secret from users WHERE secret = ?",
+            secret
+        );
 
         let result = query.fetch_optional(pool).await.expect("Must be valid");
 
         match result {
-            Some(result) => {
-                User {
-                    id: result.id,
-                    secret: secret.to_string(),
-                }
-            }
+            Some(result) => User {
+                id: result.id,
+                secret: secret.to_string(),
+            },
             None => {
                 let user = create_user(pool).await;
                 cookies.add(Cookie::new("secret", user.secret.to_owned()));
@@ -128,9 +132,7 @@ async fn ensure_auth(cookies: &Cookies,
                 user
             }
         }
-    }
-    else
-    {
+    } else {
         let user = create_user(pool).await;
         cookies.add(Cookie::new("secret", user.secret.to_owned()));
 
@@ -138,21 +140,13 @@ async fn ensure_auth(cookies: &Cookies,
     }
 }
 
-
 // Display one statement at random
-async fn index(
-    cookies: Cookies,
-    Extension(pool):Extension<SqlitePool>) -> Html<String> {
-
-    let query = sqlx::query_as::<_, Statement>("SELECT id, text from statements ORDER BY RANDOM() LIMIT 1");
+async fn index(cookies: Cookies, Extension(pool): Extension<SqlitePool>) -> Html<String> {
+    let query =
+        sqlx::query_as::<_, Statement>("SELECT id, text from statements ORDER BY RANDOM() LIMIT 1");
     let result = query.fetch_one(&pool).await.expect("Must be valid");
 
-    let template = IndexTemplate {
-        statement: &result
-    };
+    let template = IndexTemplate { statement: &result };
 
     Html(template.render().unwrap())
-
 }
-
-
