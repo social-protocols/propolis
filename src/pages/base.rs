10a4d4e3ae90f78cc129f9bcf8f874f96a634c5e
Felix Dietze <felix.dietze@rwth-aachen.de>
2023-02-25 18:48:53 +0100

    Remove themes except light and dark

--- src/pages/base.rs
+++ src/pages/base.rs
@@ -9,7 +9,7 @@ pub struct BaseTemplate {
 pub fn get_base_template(cookies: Cookies, Extension(_): Extension<SqlitePool>) -> BaseTemplate {
     let theme = cookies
         .get("theme")
-        .unwrap_or(Cookie::new("theme", "bright"));
+        .unwrap_or(Cookie::new("theme", "light"));
 
     BaseTemplate {
         theme: theme.value().to_string(),
