7ce5b01f6c13f6a0c81228b95545f7811d481fa0
Thomass Karolski <thomas.karolski@gmail.com>
2023-02-26 14:47:44 +0100

    Fix formatting

--- src/pages/options.rs
+++ src/pages/options.rs
@@ -1,4 +1,4 @@
-use super::base::{get_base_template, BaseTemplate, GenericViewTemplate, WarningDialog };
+use super::base::{get_base_template, BaseTemplate, GenericViewTemplate, WarningDialog};
 use crate::{auth::User, error::Error, util::base_url};
 use maud::html;
 
@@ -65,16 +65,23 @@ pub async fn options(
             let merge_url = format!("{}/merge/{}", base_url(&headers), &user.secret);
             let base = get_base_template(cookies, Extension(pool));
             let content = html(&base, &merge_url, qr_code_base64(&merge_url).as_str());
-            GenericViewTemplate { base, content: content.as_str(), title }.into()
+            GenericViewTemplate {
+                base,
+                content: content.as_str(),
+                title,
+            }
+            .into()
         }
         None => GenericViewTemplate {
-                base: get_base_template(cookies, Extension(pool)),
-                title,
-                content: String::from(WarningDialog {
-                    msg: "Options disabled until you cast your first vote.",
-                    ..Default::default()
-                }).as_str(),
-            }.into()
+            base: get_base_template(cookies, Extension(pool)),
+            title,
+            content: String::from(WarningDialog {
+                msg: "Options disabled until you cast your first vote.",
+                ..Default::default()
+            })
+            .as_str(),
+        }
+        .into(),
     }
 }
 
