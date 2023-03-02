b7212e909595dd729f014c08580f9cc3488248ca
Thomass Karolski <thomas.karolski@gmail.com>
2023-03-02 17:34:09 +0100

    Move js code into file

--- src/pages/statement.rs
+++ src/pages/statement.rs
@@ -31,53 +31,7 @@ pub async fn votes(
 
         div id="chart" {}
         script type="text/javascript" {
-            (PreEscaped(r###"
-         function setupChart(agree, skip, disagree) {
-             var options = {
-                 colors: ["#00FF00", "#AAAAAA", "#FF0000"],
-                 chart: {
-                     height: "80px",
-                     type: 'bar',
-                     toolbar: {
-                         show: false
-                     },
-                 },
-                 plotOptions: {
-                     bar: {
-                         borderRadius: 0,
-                         horizontal: true,
-                         distributed: true,
-                     }
-                 },
-                 xaxis: {
-                     categories: ['Agree', 'Skip', 'Disagree'],
-                     axisBorder: {
-                         show: false,
-                     },
-                     axisTicks: {
-                         show: false,
-                     },
-                     labels: {
-                         show: false,
-                     }
-                 },
-                 legend: {
-                     show: false
-                 },
-                 yaxis: {
-                     labels: {
-                         show: false
-                     }
-                 },
-                 series: [{
-                     data: [agree, skip, disagree]
-                 }],
-             }
-             var chart = new ApexCharts(document.querySelector("#chart"), options);
-             chart.render();
-         }
-"###))
-(format!("setupChart({},{},{});", a, s, d))
+            (format!("setupChart('#chart', {},{},{});", a, s, d))
         }
     }.into_string()))
 }
---
+++ static/js/utils.js
@@ -0,0 +1,45 @@
+function setupChart(selector, agree, skip, disagree) {
+    var options = {
+        colors: ["#00FF00", "#AAAAAA", "#FF0000"],
+        chart: {
+            height: "80px",
+            type: 'bar',
+            toolbar: {
+                show: false
+            },
+        },
+        plotOptions: {
+            bar: {
+                borderRadius: 0,
+                horizontal: true,
+                distributed: true,
+            }
+        },
+        xaxis: {
+            categories: ['Agree', 'Skip', 'Disagree'],
+            axisBorder: {
+                show: false,
+            },
+            axisTicks: {
+                show: false,
+            },
+            labels: {
+                show: false,
+            }
+        },
+        legend: {
+            show: false
+        },
+        yaxis: {
+            labels: {
+                show: false
+            }
+        },
+        series: [{
+            data: [agree, skip, disagree]
+        }],
+    };
+
+    var chart = new ApexCharts(document.querySelector(selector), options);
+    chart.render();
+}
