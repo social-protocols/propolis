f0e60935326331032792ccf9952b82205a2777b7
Felix Dietze <felix.dietze@rwth-aachen.de>
2023-03-22 09:36:49 +0100

    Remove apexcharts.css

--- src/pages/base.rs
+++ src/pages/base.rs
@@ -21,7 +21,6 @@ fn render_base(
                 link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png";
                 link rel="manifest" href="/site.webmanifest";
                 link rel="mask-icon" href="/safari-pinned-tab.svg" color="#da8d2b";
-                link rel="stylesheet" type="text/css" href="/css/apexcharts.css";
 
                 meta name="msapplication-TileColor" content="#2b5797";
                 meta name="theme-color" content="#ffffff";
