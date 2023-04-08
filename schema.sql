1a462266162e2b9bd7c7dff66ee80ccc7762e93f
Thomass Karolski <thomas.karolski@gmail.com>
2023-04-08 22:06:27 +0200

    Run just fix

--- schema.sql
+++ schema.sql
@@ -238,3 +238,13 @@ CREATE TRIGGER api_key_stats AFTER INSERT ON statement_predictions
        SET total_tokens = total_tokens + new.total_tokens
      WHERE id = new.api_key_id;
   END;
+CREATE TABLE statement_flags (
+  statement_id integer not null references statements(id) on delete cascade on update cascade,
+  -- fully flagged or maybe flagged
+  state integer not null,
+  -- contains serialized json
+  categories text not null,
+  -- https://stackoverflow.com/questions/11556546/sqlite-storing-default-timestamp-as-unixepoch
+  created integer not null default (strftime('%s', 'now')),
+  primary key (statement_id)
+) strict;
