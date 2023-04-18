e47bf0ad166d80b9c45856c59d7df419749597f1
Felix Dietze <felix.dietze@rwth-aachen.de>
2023-04-18 20:29:51 +0300

    Log backend error messages

--- src/error.rs
+++ src/error.rs
@@ -9,9 +9,11 @@ pub struct AppError(anyhow::Error);
 // Tell axum how to convert `AppError` into a response.
 impl IntoResponse for AppError {
     fn into_response(self) -> axum::response::Response {
+        let msg = self.0.to_string();
+        tracing::error!("{msg}");
         (
             StatusCode::INTERNAL_SERVER_ERROR,
-            format!("Something went wrong: {}", self.0),
+            format!("Something went wrong: {msg}"),
         )
             .into_response()
     }
