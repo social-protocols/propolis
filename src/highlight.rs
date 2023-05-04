8f532371039ad4a99884dda99c4ee383ccbdee27
Felix Dietze <felix.dietze@rwth-aachen.de>
2023-05-04 22:48:46 +0200

    fix

--- src/highlight.rs
+++ src/highlight.rs
@@ -10,7 +10,7 @@ const HIGHLIGHT_SEARCH_PATTERNS: &[&str; 2] = &[HIGHLIGHT_BEGIN, HIGHLIGHT_END];
 const HIGHILIGHT_REPLACE_PATTERNS: &[&str; 2] = &["<mark>", "</mark>"];
 // AhoCorasick is an automaton for searching multiple strings in linear time.
 // We'll reuse the same instance for all highlighting.
-static AC: Lazy<AhoCorasick> = Lazy::new(|| AhoCorasick::new(HIGHLIGHT_SEARCH_PATTERNS));
+static AC: Lazy<AhoCorasick> = Lazy::new(|| AhoCorasick::new(HIGHLIGHT_SEARCH_PATTERNS).unwrap());
 
 // function to replace highlight markers with HTML tags
 pub fn highlight_html(text: &str) -> String {
