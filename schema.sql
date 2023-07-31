58b1c33554feb89708e8f22e80ce1fa4261e3157
Felix Dietze <felix.dietze@rwth-aachen.de>
2023-07-31 23:15:34 +0200

    wip

--- schema.sql
+++ schema.sql
@@ -1,4 +1,5 @@
 CREATE INDEX queue_statement_id on queue (user_id, created, statement_id);
+CREATE INDEX subscriptions_idx_statement_id ON subscriptions(statement_id);
 CREATE INDEX vote_history_statement_id on vote_history (user_id, created, statement_id);
 CREATE TABLE alternatives (
   statement_id integer not null references statements (id) on delete cascade on update cascade,
@@ -212,21 +213,21 @@ CREATE VIEW statement_stats AS
 WITH counted_votes as (
     SELECT
         id as statement_id
-        , sum(vote = 1) as yes_votes
-        , sum(vote = -1) as no_votes
-        , sum(vote = 0) as skip_votes
-        , sum(vote = 2) as itdepends_votes
-    from statements s
-    join votes v
-    on s.id = v.statement_id
+        , coalesce(sum(vote = 1), 0) as yes_votes
+        , coalesce(sum(vote = -1), 0) as no_votes
+        , coalesce(sum(vote = 0), 0) as skip_votes
+        , coalesce(sum(vote = 2), 0) as itdepends_votes
+    from statements
+    left outer join votes
+    on statements.id = votes.statement_id
     group by statement_id
 )
 , counted_votes_subscriptions as (
     select
-        *
-        , count(*) as subscriptions
-    from counted_votes c
-    join subscriptions s
+        counted_votes.*
+        , count(all user_id) as subscriptions
+    from counted_votes
+    left outer join subscriptions
     using(statement_id)
     group by statement_id
 )
@@ -238,10 +239,10 @@ WITH counted_votes as (
 )
 select
     *
-    , (cast(total_votes - skip_votes as real) / (total_votes)) as participation
-    , (1.0 - cast((abs(yes_votes - no_votes)) as real) / (total_votes - skip_votes)) as polarization
-    , (cast(total_votes - skip_votes as real) / (subscriptions)) as votes_per_subscription
+    , coalesce((cast(total_votes - skip_votes as real) / (total_votes)), 0) as participation
+    , coalesce((1.0 - cast((abs(yes_votes - no_votes)) as real) / (total_votes - skip_votes)), 0) as polarization
+    , coalesce((cast(total_votes - skip_votes as real) / (subscriptions)), 0) as votes_per_subscription
 from cte
-/* statement_stats(statement_id,yes_votes,no_votes,skip_votes,itdepends_votes,user_id,created,subscriptions,total_votes,participation,polarization,votes_per_subscription) */;
+/* statement_stats(statement_id,yes_votes,no_votes,skip_votes,itdepends_votes,subscriptions,total_votes,participation,polarization,votes_per_subscription) */;
 CREATE VIRTUAL TABLE statements_fts USING fts5(id UNINDEXED, text)
 /* statements_fts(id,text) */;
